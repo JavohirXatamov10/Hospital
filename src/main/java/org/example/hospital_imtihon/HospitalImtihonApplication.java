4654a78f80902f522b5d0a8af99bd8335f144cb1
JavohirXatamov10 <xatamovjavohir70@gmail.com>
2024-06-10 18:19:55 +0500

    begin

---
+++ src/main/java/org/example/hospital_imtihon/HospitalImtihonApplication.java
@@ -0,0 +1,13 @@
+package org.example.hospital_imtihon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalImtihonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalImtihonApplication.class, args);
+    }
+
+}
